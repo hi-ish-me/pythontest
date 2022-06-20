7235019bf36db4216edf7a01a6c41a3522f52057
hi-ish-me <chatdaemoness@gmail.com>
2022-06-20 19:56:02 +0530

    "Modified to index version 1.2"

---
+++ Test1.py
@@ -0,0 +1,15 @@
+print("Hello World")
+
+if 5 > 2:
+    print("5 greater than 2")
+else:
+    print("Bullshit")
+    print("Bullshit 2")
+
+print("Hello Second World")
+
+
+
+
+if 10< 20:
+    print("10 is less than 20")
